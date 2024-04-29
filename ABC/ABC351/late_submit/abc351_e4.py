1b4e1ec42980fefcb6f3478b3cc7e919d18cab56
hira0912 <hira0912@gmail.com>
2024-04-30 00:42:16 +0900

    2024/4/30 ABC/ABC351 problem E late submit(pypy)

---
+++ ABC/ABC351/late_submit/abc351_e4.py
@@ -0,0 +1,35 @@
+def function():
+    # 標準入力
+    n = int(input())
+    a1 = []
+    a2 = []
+
+    # x+yの2で割った余りが異なる点同士は到達不可能なので、
+    # それらを先に組み分けしておく
+    for _ in range(n):
+        a = list(map(int,input().split()))
+        if((a[0]+a[1])%2 == 0):
+            a1.append(a)
+        else:
+            a2.append(a)
+
+    # 計算関数の定義
+    def sumup(a):
+        X = []
+        Y = []
+        num = 0
+        for c in range(len(a)):
+            X.append(a[c][0]+a[c][1])
+            Y.append(a[c][0]-a[c][1])
+        X.sort()
+        Y.sort()
+        XY_len = len(X)
+        for i in range(XY_len):
+            num += (2*i + 1 - XY_len) * (X[i] + Y[i]) // 2
+        return num
+
+    print(sumup(a1)+sumup(a2))
+    return
+
+if __name__ == "__main__":
+    function()
