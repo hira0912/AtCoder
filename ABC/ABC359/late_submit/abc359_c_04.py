e809ea4410f24345575de2bf2df1e7d9de52e1ec
hira0912 <hira0912@gmail.com>
2024-06-27 17:14:24 +0900

    ABC/ABC359 problem C update

--- ABC/ABC359/late_submit/abc359_c_04.py
+++ ABC/ABC359/late_submit/abc359_c_04.py
@@ -4,33 +4,35 @@ def function():
     T = list(map(int,input().split()))
 
     # 各座標系における距離の計算
+    dy = abs(T[1]-S[1])
+    dx = abs(T[0]-S[0])
+
     # もし点間距離がx座標系よりy座標系の方が大きければ、y座標の差を出力する
-    ydt = abs(T[1]-S[1])
-    xdt = abs(T[0]-S[0])
-    if(ydt>=xdt):
-        print(ydt)
+    if(dy>=dx):
+        print(dy)
         return
     
-    # 点Sと点Tの内、X座標にて小さい方をA、大きい方をBとする
+    # 点Sと点Tの内、X座標にて小さい方をPoint_L、大きい方をPoint_Rとする
     if(S[0]<=T[0]):
-        A = S
-        B = T
+        Point_L = S
+        Point_R = T
     else:
-        A = T
-        B = S
+        Point_L = T
+        Point_R = S
 
-    # 点A,Bをそれぞれ同一タイル内の右側に寄せる
-    if(A[0]+A[1])%2 == 0:
-        A[0] += 1
-    if(B[0]+B[1])%2 == 0:
-        B[0] += 1
+    # 点Point_L,Point_Rをそれぞれ同一タイル内の右側に寄せる
+    if(Point_L[0]+Point_L[1])%2 == 0:
+        Point_L[0] += 1
+    if(Point_R[0]+Point_R[1])%2 == 0:
+        Point_R[0] += 1
     
-    # 点Aを点Bと同じy軸まで移し、同一タイル内の右側に寄せる
-    A[0] += ydt
-    if(A[0]+B[1])%2 == 0:
-        A[0] += 1
+    # 点Point_Lから点Point_Rと同じy軸まで移動し、その後同一タイル内の右側に寄せる
+    Point_L[0] += dy
+    if(Point_L[0]+Point_R[1])%2 == 0:
+        Point_L[0] += 1
 
-    ans = ydt+(B[0]-A[0])//2
+    # x軸の差分の半分とy軸の移動分の和を出力
+    ans = (Point_R[0]-Point_L[0])//2 + dy
     print(ans)
     return            
 
