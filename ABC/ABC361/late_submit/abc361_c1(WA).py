a7b4e117255e98c10ca43e7d683a5989743f7f5f
hira0912 <hira0912@gmail.com>
2024-07-07 00:02:04 +0900

    ABC/ABC361 problem C updated(Comment)

--- ABC/ABC361/late_submit/abc361_c1(WA).py
+++ ABC/ABC361/late_submit/abc361_c1(WA).py
@@ -1,14 +1,17 @@
 def function():
+    # 標準入力
     N,K = map(int,input().split())
     A = list(map(int,input().split()))
     A.sort()
 
+    # 配列Aの差配列Bを生成
     B = []
     A_bef = A[0]
     for i in range(1,N):
         B.append(A[i]-A_bef)
         A_bef = A[i]
     
+    # 差配列の両端を比べ、大きい方を中央に1つ進める
     left_key = 0
     right_key = N-1
     for _ in range(K):
@@ -16,6 +19,8 @@ def function():
             left_key += 1
         else:
             right_key -= 1
+    
+    # 出力
     print(A[right_key]-A[left_key])
     return
 
--- ABC/ABC361/late_submit/abc361_c2.py
+++ ABC/ABC361/late_submit/abc361_c2.py
@@ -1,16 +1,22 @@
 def function():
+    # 標準入力
     N,K = map(int,input().split())
     A = list(map(int,input().split()))
     A.sort()
 
+    # 初期区間設定
     ans = 10**9
     left_key = 0
     right_key = N-K-1
+
+    # 区間を右に１個ずつずらす
     while(right_key < N):
         if(A[right_key]-A[left_key] < ans):
             ans = A[right_key]-A[left_key]
         left_key += 1
         right_key += 1
+    
+    # 出力
     print(ans)
     return
 
