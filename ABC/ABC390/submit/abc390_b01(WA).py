8ad46e80c6fa2623448c88ea485aea3f8ff95d1e
hira0912 <hira0912@gmail.com>
2025-01-25 22:40:20 +0900

    2025/01/25 ABC/ABC390 problem B

---
+++ ABC/ABC390/submit/abc390_b01(WA).py
@@ -0,0 +1,15 @@
+def function():
+    N = int(input())
+    A = list(map(int,input().split()))
+
+    k = A[1]/A[0]
+
+    for i in range(1,N):
+        if(A[i]/A[i-1] != k):
+            print('No')
+            return
+    print('Yes')
+    return
+
+if __name__ == "__main__":
+    function()
