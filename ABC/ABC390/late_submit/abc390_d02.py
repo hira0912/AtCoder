212e6f96d21aadeff391ac5a1b5a21e649b15d60
hira0912 <hira0912@gmail.com>
2025-01-28 22:35:19 +0900

    2025/01/28 ABC/ABC390 problem D resolve

---
+++ ABC/ABC390/late_submit/abc390_d02.py
@@ -0,0 +1,33 @@
+def function():
+    global N,A,S,B
+    # input
+    N = int(input())
+    A = list(map(int,input().split()))
+    # global variables
+    S = [0]*12
+    B = set()
+    # calculation
+    calc(0,0)
+    print(len(B))
+    return
+
+def calc(index:int, size:int) -> None:
+    if(index>=N):
+        B.add(xor(S))
+        return
+    for i in range(size+1):
+        S[i] += A[index]
+        if(i<size):
+            calc(index+1,size)
+        else:
+            calc(index+1,size+1)
+        S[i] -= A[index]           
+
+def xor(A:list)->int:
+    ret = A[0]
+    for i in range(1,len(A)):
+        ret = ret^A[i]
+    return ret
+
+if __name__ == "__main__":
+    function()
