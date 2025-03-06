b11c126151fc4ddb1538575be02f650208a34fff
hira0912 <hira0912@gmail.com>
2025-03-06 23:52:56 +0900

    2025/03/06 ABC/ABC395 problem A

---
+++ ABC/ABC395/README.md
@@ -0,0 +1,7 @@
+# AtCoder Beginner Contest 395
+
+## contest date 
+2024/03/01 21:00 ~ 22:40
+
+## URL
+https://atcoder.jp/contests/abc395
---
+++ ABC/ABC395/late_submit/abc395_a.py
@@ -0,0 +1,13 @@
+def function():
+    N = int(input())
+    A = list(map(int,input().split()))
+
+    for i in range(1,N):
+        if(A[i]<=A[i-1]):
+            print('No')
+            return
+    print('Yes')
+    return
+
+if __name__ == "__main__":
+    function()
